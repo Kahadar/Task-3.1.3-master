7c6ed750f7fae334f000c6b5b1c789f3d25b2685
Kahadar <solntsedar72@gmail.com>
2023-01-25 15:09:07 +0300

    Done

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/model/User.java
@@ -0,0 +1,99 @@
+package ru.itmentor.spring.boot_security.demo.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+
+public class User implements UserDetails {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @Size(min = 2, max = 50, message = "Name length should from 2 to 50 characters")
+    private String name;
+
+    @Size(min = 2, message = "Password at least 2 characters")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    @Min(value = 1, message = "Minimal age is 1")
+    private int age;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roleList;
+
+
+    public void setRole(Role role){
+        if(roleList == null) {
+            roleList = new ArrayList<>();
+        }
+        roleList.add(role);
+    }
+
+
+    public String getShotRoles() {
+        return roleList.toString().replace(","," ").replaceAll("[^A-Za-zА-Яа-я0-9,' ']", "");
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoleList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
